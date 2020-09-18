f92156df752c6094f347094b2815e353ff3f6054
Tom Oliveira <34668864+tomoliveirabastos@users.noreply.github.com>
2020-09-18 15:51:35 -0300

    add rota de teste

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.get("/teste_app", (req, res)=>{
+    res.json({"teste": "ok"});
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
