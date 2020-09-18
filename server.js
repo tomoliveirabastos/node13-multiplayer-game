e2f7db5244e09c5c5d47578fb940f7200808cd86
Tom Oliveira <34668864+tomoliveirabastos@users.noreply.github.com>
2020-09-18 15:58:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get("/teste_app", (req, res)=>{
     res.json({"teste": PORT});
